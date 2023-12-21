180ac6c42f8412aca1159088627cef015b618948
szebestik@gmail.com <szebestik@gmail.com>
2023-12-21 10:44:39 +0100

    Fixed add comment not updating validity problem

--- src/modules/VideoPage/components/add-video-comment/add-video-comment.tsx
+++ src/modules/VideoPage/components/add-video-comment/add-video-comment.tsx
@@ -17,41 +17,35 @@ export type AddVideoCommentProps = {
 }
 
 export const AddVideoComment = ({ videoId, user, loadingComments }: AddVideoCommentProps) => {
-	const [addVideoComment, { reset: resetMutation }] = useAddVideoCommentMutation();
+	const [addVideoComment, { isLoading }] = useAddVideoCommentMutation();
 
 	const {
 		register,
 		handleSubmit,
 		reset,
-		formState: { isSubmitted, isValid }
-	} = useForm();
+		formState: { isValid }
+	} = useForm<AddVideoCommentModel>({ defaultValues: { data: "" } });
 
-	const submit = async (form: AddVideoCommentModel) => {
-		try {
-			await addVideoComment({
-				videoId,
-				body: form,
-				user
-			});
-		}
-		finally {
-			setTimeout(() => {
-				resetMutation();
-				reset();
-			}, 0);
-		}
+	const submit = (body: AddVideoCommentModel) => {
+		reset();
+
+		addVideoComment({
+			videoId,
+			body,
+			user
+		});
 	}
 
 	return (
 		<div className={styles.container}>
 			<ProfilePicture src={user.profilePictureSrc} />
-			<Form className={styles.container__form} onSubmit={handleSubmit((form) => submit(form as AddVideoCommentModel))}>
+			<Form className={styles.container__form} onSubmit={handleSubmit(submit)}>
 				<Form.Group controlId="description">
 					<Form.Control as="textarea" disabled={loadingComments} type="text" {...register('data', { required: true })} className={styles.form__textarea} />
 				</Form.Group>
 				<div className={styles.container__form__buttons}>
-					<button type="button" disabled={!isValid || isSubmitted || loadingComments} className="btn btn-secondary btn-md" onClick={reset}>Cancel</button>
-					<button type="submit" disabled={!isValid || isSubmitted || loadingComments} className="btn btn-primary btn-md">Comment</button>
+					<button type="button" disabled={!isValid || isLoading || loadingComments} className="btn btn-secondary btn-md" onClick={() => reset()}>Cancel</button>
+					<button type="submit" disabled={!isValid || isLoading || loadingComments} className="btn btn-primary btn-md">Comment</button>
 				</div>
 			</Form>
 		</div>

66b0e546ace86986926deacd0cbea9965146d1d6
Mateusz Szebestik <szebestik@gmail.com>
2024-01-13 12:02:00 +0100

    Check if user is logged in while likeing/dislikeing a video

--- src/modules/VideoPage/components/video-likes/video-likes.tsx
+++ src/modules/VideoPage/components/video-likes/video-likes.tsx
@@ -1,10 +1,12 @@
 import { Button, ButtonGroup } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 import styles from "./video-likes.module.scss";
 
 import { formatNumbers } from "src/modules/shared/helpers";
 
 import { useDeleteLikeFromVideoMutation, useLikeVideoMutation } from "../../api/videoApiSlice";
+import { useAuth } from "src/modules/shared/providers";
 
 import { VideoDetails } from "../../models";
 
@@ -14,27 +16,47 @@ export type VideoLikesProps = {
 };
 
 export const VideoLikes = ({ data, videoId }: VideoLikesProps) => {
+	const { user } = useAuth();
+
 	const [likeVideo, { isLoading: isLikeVideoLoading }] = useLikeVideoMutation();
 	const [deleteLikeFromVideo, { isLoading: isDeleteLikeLoading }] = useDeleteLikeFromVideoMutation();
 
 	const handleLike = () => {
+		if (!checkLoggedInStatus()) return;
+
 		likeVideo({ videoId, value: 1 });
 	}
 
 	const handleDislike = () => {
+		if (!checkLoggedInStatus()) return;
+
 		likeVideo({ videoId, value: -1 });
 	}
 
 	const handleRemoveLikeOrDislike = () => {
+		if (!checkLoggedInStatus()) return;
+
 		deleteLikeFromVideo({ videoId, isLike: data.isLiked });
 	}
+
+	const checkLoggedInStatus = () => {
+		if (user === undefined) {
+			toast("You must be logged in to like a video")
+
+			return false;
+		}
+
+		return true;
+	}
+
+	const isDisabled = isLikeVideoLoading || isDeleteLikeLoading
 
 	return (
 		<ButtonGroup className={styles.container}>
 			<Button
 				className={`${data.isLiked ? "btn-dark" : "btn-light"} btn-lg btn-pill`}
 				onClick={data.isLiked ? handleRemoveLikeOrDislike : handleLike}
-				disabled={isLikeVideoLoading || isDeleteLikeLoading}
+				disabled={isDisabled}
 			>
 				<i className={`bi bi-hand-thumbs-up${data.isLiked ? '-fill' : ''}`}></i>
 				{formatNumbers(data.likes, data.likes >= 10000 ? 0 : 1)}
@@ -49,7 +71,7 @@ export const VideoLikes = ({ data, videoId }: VideoLikesProps) => {
 			<Button
 				className={`${data.isDisliked ? "btn-dark" : "btn-light"} btn-lg btn-pill`}
 				onClick={data.isDisliked ? handleRemoveLikeOrDislike : handleDislike}
-				disabled={isLikeVideoLoading || isDeleteLikeLoading}
+				disabled={isDisabled}
 			>
 				<i className={`bi bi-hand-thumbs-down${data.isDisliked ? '-fill' : ''}`}></i>
 				{formatNumbers(data.dislikes, data.dislikes >= 10000 ? 0 : 1)}

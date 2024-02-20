912585d77d9f128e13e890cae73d588ffff2b190
Mateusz Szebestik <szebestik@gmail.com>
2024-02-20 15:41:51 +0100

    Change BE url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://taitube-backend.onrender.com/';
+export const API_BASE_URL = 'https://taitube-backend.onrender.com';
 
 export const IN_VIEW_LOCAL_STORAGE_KEY = 'IN_VIEW';
